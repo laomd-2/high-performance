9a8cfc944f8d2f0dfcefe445d146a15ca5fabaa7
laomd-2 <laomd@mail2.sysu.edu.cn>
2019-01-22 15:29:59 +0800

    CUDA 32*32

--- lab10/cuda/main.cpp
+++ lab10/cuda/main.cpp
@@ -10,27 +10,26 @@
 #include "csrspmat.h"
 //#include "../util.cuh"
 using namespace std;
-
-#ifndef WARP_SIZE
-#define WARP_SIZE 32
-#endif
-
 #define FULL_MASK 0xffffffff
-#define nWarps 1
 
 #ifndef N
 #define N 32
+#endif
+
+#ifndef nWarps
+//#define nWarps ((48 * 1024) / (N + 2))
+#define nWarps 2
 #endif
 
 __global__ void kernel_multiply(const float* a_data, const int* r1, const int* c1,
                                 const float* b_data, const int* r2, const int* c2) {
     __shared__ float sValA[nWarps];
     __shared__ int sColA[nWarps];
-    __shared__ float result_row[nWarps * N];
+    __shared__ float result_row[nWarps][N];
 
     int blockId = blockIdx.x;
-    int warpId = threadIdx.x / WARP_SIZE;
-    int laneId = threadIdx.x % WARP_SIZE;
+    int warpId = threadIdx.x / warpSize;
+    int laneId = threadIdx.x % warpSize;
     int globalWarpId = blockId * nWarps + warpId;
 
     int aColIt = r1[globalWarpId] + laneId;
@@ -42,8 +41,8 @@ __global__ void kernel_multiply(const float* a_data, const int* r1, const int* c
     int colB;
     float valB;
 
-    for (int i = laneId; i < N; i += WARP_SIZE)
-        result_row[warpId * N + i] = 0.0f;
+    for (int i = laneId; i < N; i += warpSize)
+        result_row[warpId][i] = 0.0f;
 
     for(int k = 0, end = __popc(__ballot_sync(FULL_MASK, aColIt < aColEnd)); k < end; ++k)
     {
@@ -56,17 +55,17 @@ __global__ void kernel_multiply(const float* a_data, const int* r1, const int* c
         bColIt = r2[sColA[warpId]] + laneId; // sColA is volatile and warp’s threads
         bColEnd = r2[sColA[warpId] + 1]; // are implicitly synchronized
 
-        for(; __any_sync(FULL_MASK, bColIt < bColEnd ); bColIt += 32) {
+        for(; __any_sync(FULL_MASK, bColIt < bColEnd ); bColIt += warpSize) {
             colB = bColIt < bColEnd ? c2[bColIt] : -1;
             valB = bColIt < bColEnd ? b_data[bColIt] : 0.0f;
             if (colB > -1)
-                result_row[colB] += sValA[warpId] * valB;       // colB必不同，避免bank conflict
+                result_row[warpId][colB] += sValA[warpId] * valB;       // colB必不同，避免bank conflict
         }
     }
 
-    if (globalWarpId == 0) {
-        for (int i = laneId; i < N; i += WARP_SIZE)
-            printf("(%d %d %lf) ", globalWarpId, i, result_row[warpId * N + i]);
+    if (globalWarpId == nWarps - 1) {
+        for (int i = laneId; i < N; i += warpSize)
+            printf("(%d %d %lf) ", globalWarpId, i, result_row[warpId][i]);
     }
 }
 
@@ -90,7 +89,7 @@ int main() {
 
     DeviceCsrSpMat a(mata), b(matb);
 
-    kernel_multiply<<<1, nWarps * WARP_SIZE>>>(a.data, a.row_indices, a.col_indices,
+    kernel_multiply<<<1, nWarps * 32>>>(a.data, a.row_indices, a.col_indices,
             b.data, b.row_indices, b.col_indices);
     cudaDeviceReset();
 }
